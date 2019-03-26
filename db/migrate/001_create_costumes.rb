f47eaa1a7ef2893e7b77f5ffdb683cf92e167df5
Vinh Do <vinhdo07@gmail.com>
2019-03-26 03:15:44 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,7 +10,11 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table  :costumes do |t|
-      
+      t.string :name
+      t.float :price
+      t.string :url
+      t.string :size
+      t.timestamps
     end
   end
 end
