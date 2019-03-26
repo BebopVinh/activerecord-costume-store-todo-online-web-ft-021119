012798f25cdc75b2b0cc1be8e97e406a4f830ad5
Vinh Do <vinhdo07@gmail.com>
2019-03-26 03:28:13 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,6 +5,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :location
       t.string :theme
       t.float :price
-      t.boolean :family_friendly  
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date  
   end
 end
